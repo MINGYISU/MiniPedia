66545ed1ac3311b6ac2d427f044cc6ee6e73126a
MINGYI SU <125218610+MINGYISU@users.noreply.github.com>
2024-08-04 19:27:29 -0400

    Updated urls.py

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     path("", views.index, name="index"), 
     path("search", views.search, name="search"), 
     path("newpage", views.create, name="create"), 
-    path("edit/<str:name>", views.edit, name="edit"), 
-    path("<str:name>", views.get, name="get"), 
-    
-    
+    path("<str:name>/edit", views.edit, name="edit"), 
+    path("<str:name>", views.get, name="get")
 ]
